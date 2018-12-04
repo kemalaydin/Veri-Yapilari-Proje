24fe72ea31ef937481a10e36fbccdddae62313a6
Kemal <kmlcanaydin@gmail.com>
2018-12-04 16:22:41 +0300

    son hali

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
-//
-//  main.cpp
-//  CiftBaglantili
-//
-//  Created by Kemal on 25.11.2018.
-//  Copyright © 2018 Kemal. All rights reserved.
-//
-
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -54,143 +48,140 @@ public:
     Node* prev;
 };
 
-class List{
-    public:
-        List(void){ head = NULL; }
-        ~List(void){
-            {
-                /* Node* currNode = head, *nextNode = NULL;
-                while (currNode != NULL)
-                {
-                    nextNode    =    currNode->next;
-                    delete currNode;
-                    currNode    =    nextNode;
-                } */
+class LinkedList{
+public:
+    LinkedList(void){ head = NULL; }
+    ~LinkedList(void){
+        {
+            /* Node* currNode = head, *nextNode = NULL;
+             while (currNode != NULL)
+             {
+             nextNode    =    currNode->next;
+             delete currNode;
+             currNode    =    nextNode;
+             } */
+        }
+    };
+
+    Node* InsertNode(int index, student student){
+        if (index < 0) return NULL;
+
+        int currIndex  = 1;
+        Node* currNode = head;
+        while (currNode && index > currIndex) {
+            currNode = currNode->next;
+            currIndex++;
+        }
+        if (index > 0 && currNode == NULL) return NULL;
+
+        Node* newNode = new Node;
+        newNode->data = student;
+        if (index == 0) {
+            newNode->next = NULL;
+            newNode->prev = NULL;
+            head = newNode;
+        } else {
+            newNode->next = currNode->next;
+            newNode->prev = currNode;
+            // eğer son halkaya eklendiyse, devamı olmayacağı için prev değeri atamasına gerek yok.
+            if(newNode->next != NULL){
+                currNode->next->prev = newNode;
             }
-        };
-
-        bool IsEmpty(){ return head == NULL; }
-
-        Node* InsertNode(int index, student student){
-            if (index < 0) return NULL;
-
-            int currIndex  = 1;
-            Node* currNode = head;
-            while (currNode && index > currIndex) {
-                currNode = currNode->next;
-                currIndex++;
-            }
-            if (index > 0 && currNode == NULL) return NULL;
-
-            Node* newNode = new Node;
-            newNode->data = student;
-            if (index == 0) {
-                newNode->next = head;
-                newNode->prev = NULL;
-                head = newNode;
-            } else {
-                newNode->next = currNode->next;
-                newNode->prev = currNode;
-                // eğer son halkaya eklendiyse, devamı olmayacağı için prev değeri atamasına gerek yok.
-                if(newNode->next != NULL){
-                    currNode->next->prev = newNode;
-                }
-                currNode->next = newNode;
-            }
-
-            alert("Öğrenci Başarıyla Kayıt Edildi","success");
-            return newNode;
-        };
-
-        int FindNode(string student_number){
-            Node* currNode = head;
-            int currIndex = 1;
-            while (currNode && currNode->data.student_number != student_number) {
-                currNode = currNode->next;
-                currIndex++;
-            }
-            if (currNode) return currIndex;
-            return 0;
-        };
-
-        int DeleteNode(string student_number){
-            Node* prevNode = NULL;
-            Node* currNode = head;
-            Node* nextNode = NULL;
-            int currIndex = 1;
-            while (currNode && currNode->data.student_number != student_number) {
-                prevNode = currNode;
-                currNode = currNode->next;
-                // Silinecek node'dan sonraki node'u alıp prev değeri değiştirilecek.
-                nextNode = currNode->next;
-                currIndex++;
-            }
-            if (currNode) {
-                if (prevNode) {
-                    prevNode->next = currNode->next;
-                    // Silinen node için önceki değerini, sonraki node 'a aktardık.
-                    nextNode->prev = currNode->prev;
-                    delete currNode;
-                }
-                else {
-                    head = currNode->next;
-                    nextNode = currNode->next;
-                    // sonraki adresin prev adresini NULL yapıyoruz. Çünkü artık ilk node.
-                    nextNode->prev =  NULL;
-                    delete currNode;
-                }
-                alert("Öğrenci Başarıyla Silindi","warning");
-                return currIndex;
-            }
-            alert("Öğrenci Bulunamadı","danger");
-            return 0;
-
-        };
-
-        int TotalNode(){
-            Node* currNode = head;
-            int index;
-            while(currNode){
-                currNode = currNode->next;
-                index++;
-            }
-            return index;
+            currNode->next = newNode;
         }
 
-        void DisplayList(void){
-            Node* currNode = head;
-            alert("Bilgisayar Programlama Dersi Öğrencileri","info");
-            while(currNode != NULL){
-                string selected_program;
-                if(currNode->data.program  == "1"){
-                    selected_program = "Bilgisayar Mühendisliği";
-                }else if(currNode->data.program == "2"){
-                    selected_program = "Makine Mühendisliği";
-                }else if(currNode->data.program == "3"){
-                    selected_program = "Gıda Mühendisliği";
-                }else{
-                    selected_program = "Tanımlı Olmayan Program";
-                }
-                cout << currNode->data.student_number + " - " + currNode->data.name + " " + currNode->data.surname + " - " + selected_program << endl;
-                currNode = currNode->next;
+        alert("Öğrenci Başarıyla Kayıt Edildi","success");
+        return newNode;
+    };
+
+    int FindNode(string student_number){
+        Node* currNode = head;
+        int currIndex = 1;
+        while (currNode && currNode->data.student_number != student_number) {
+            currNode = currNode->next;
+            currIndex++;
+        }
+        if (currNode) return currIndex;
+        return 0;
+    };
+
+    int DeleteNode(string student_number){
+        Node* prevNode = NULL;
+        Node* currNode = head;
+        Node* nextNode = NULL;
+        int currIndex = 1;
+        while (currNode && currNode->data.student_number != student_number) {
+            prevNode = currNode;
+            currNode = currNode->next;
+            // Silinecek node'dan sonraki node'u alıp prev değeri değiştirilecek.
+            nextNode = currNode->next;
+            currIndex++;
+        }
+        if (currNode) {
+            if (prevNode) {
+                prevNode->next = currNode->next;
+                // Silinen node için önceki değerini, sonraki node 'a aktardık.
+                nextNode->prev = currNode->prev;
+                delete currNode;
             }
-            cout << " ----------------- " << endl;
-        };
+            else {
+                head = currNode->next;
+                nextNode = currNode->next;
+                // sonraki adresin prev adresini NULL yapıyoruz. Çünkü artık ilk node.
+                nextNode->prev =  NULL;
+                delete currNode;
+            }
+            alert("Öğrenci Başarıyla Silindi","warning");
+            return currIndex;
+        }
+        alert("Öğrenci Bulunamadı","danger");
+        return 0;
 
-    private:
-        Node* head;
+    };
+
+    int TotalNode(){
+        Node* currNode = head;
+        int index;
+        while(currNode){
+            currNode = currNode->next;
+            index++;
+        }
+        return index;
+    }
+
+    void DisplayList(void){
+        Node* currNode = head;
+        alert("Bilgisayar Programlama Dersi Öğrencileri","info");
+        while(currNode != NULL){
+            string selected_program;
+            if(currNode->data.program  == "1"){
+                selected_program = "Bilgisayar Mühendisliği";
+            }else if(currNode->data.program == "2"){
+                selected_program = "Makine Mühendisliği";
+            }else if(currNode->data.program == "3"){
+                selected_program = "Gıda Mühendisliği";
+            }else{
+                selected_program = "Tanımlı Olmayan Program";
+            }
+            cout << currNode->data.student_number + " - " + currNode->data.name + " " + currNode->data.surname + " - " + selected_program << endl;
+            currNode = currNode->next;
+        }
+        cout << " ----------------- " << endl;
+    };
+
+    void heapSort(){
+        Node* tmp;
+        Node* currNode = head;
+        Node* newList;
+        while(currNode != NULL){
+
+        }
+    }
+private:
+    Node* head;
 };
 
-class BTree{
-    public:
-        BTree(void){ head = NULL; }
-        ~BTree(void);
-
-    private:
-        Node* head;
-};
-
-List list;
+LinkedList list;
 
 void programmingLesson(int operatingType){
 
@@ -272,7 +263,37 @@ void programmingLesson(int operatingType){
     }
 }
 
+class sortingAlgorithms{
+public:
+    void selectedAlgorithm(int type){
+        switch (type){
+            case 1:{
+                break;
+            }
+            case 4:{
+                list.heapSort();
+            }
+        }
+    }
+
+};
+
+void sortingMenu(){
+    int selected_sorting;
+    cout << "Sıralama İşlemi Seçiniz" << endl << "-------------" << endl;
+    cout << "1-) Hızlı Sıralama ( Bilgisayar Programlama )" << endl;
+    cout << "2-) Hızlı Sıralama ( Veritabanı Yönetim Sistemleri )" << endl;
+    cout << "3-) Radiks Sıralama ( Veritabanı Yönetim Programlama )" << endl;
+    cout << "4-) Kümeleme Sıralama ( Bilgisayar Programlama )" << endl;
+    cout << "Seçiminiz : ";
+    cin >> selected_sorting;
+    sortingAlgorithms sorting;
+    sorting.selectedAlgorithm(selected_sorting);
+}
+
+
 int main(int argc, const char * argv[]) {
+
     int operatingType = NULL;
     int lessonType = NULL;
     student deneme;
@@ -284,15 +305,15 @@ int main(int argc, const char * argv[]) {
     list.DisplayList();
     do{
         cout << "İşlem Yapmak İstediğiniz Dersi Seçiniz ; " << endl;
-        cout << "1 - Bilgisayar Programlama" << endl << "2 - Veritabanı Yönetim Sistemleri" << endl << "3- Çıkış" << endl;
+        cout << "1 - Bilgisayar Programlama" << endl << "2 - Veritabanı Yönetim Sistemleri" << endl << "3- Karşılaştırma" << endl << "4- Sıralama" << endl << "5- Çıkış" << endl;
         cout << "Seçilen Ders : ";
         cin >> lessonType;
         switch (lessonType) {
             case 1:
                 cout << "----------" << endl << "Bilgisayar Programlama Dersi İçin Yapmak İstediğiniz İşlemi Seçiniz ; "
-                     << endl;
+                << endl;
                 cout << "1- Öğrenci Ekle" << endl << "2- Öğrenci Sil" << endl << "3- Öğrenci Bul" << endl
-                     << "4- Öğrencileri Listele" << endl << "5- Otomatik Öğrenci Ekle" << endl;
+                << "4- Öğrencileri Listele" << endl << "5- Otomatik Öğrenci Ekle" << endl;
                 cout << "Seçilen İşlem : ";
                 cin >> operatingType;
                 programmingLesson(operatingType);
@@ -303,6 +324,13 @@ int main(int argc, const char * argv[]) {
                 break;
 
             case 3:
+                cout << "Karşılaştırmalar Yapılacak" << endl;
+                break;
+
+            case 4:
+                    sortingMenu();
+                break;
+            case 5:
                 alert("İyi Çalışmalar","warning");
                 break;
             default:
@@ -310,55 +338,6 @@ int main(int argc, const char * argv[]) {
                 break;
         }
     }while(lessonType != 3);
-    /*
-    List list;
-    student deneme;
-    deneme.student_number = "1030516799";
-    deneme.name = "Kemal";
-    deneme.surname = "Aydın";
-    deneme.program = "Bilgisayar Müh";
-    
-    student deneme2;
-    deneme2.student_number = "1030516798";
-    deneme2.name = "Mehmet";
-    deneme2.surname = "Kaynak";
-    deneme2.program = "Bilgisayar Mühendisliği";
-    
-    student deneme3;
-    deneme3.student_number = "1030516797";
-    deneme3.name = "Ali";
-    deneme3.surname = "Çolak";
-    deneme3.program = "Bil. Müh.";
-    
-    student deneme4;
-    deneme4.student_number = "00000";
-    deneme4.name = "Ali";
-    deneme4.surname = "Çolak";
-    deneme4.program = "Bil. Müh.";
-    
-    student deneme5;
-    deneme5.student_number = "11111";
-    deneme5.name = "Ali";
-    deneme5.surname = "Çolak";
-    deneme5.program = "Bil. Müh.";
-    
-    list.InsertNode(0, deneme);
-    list.InsertNode(1, deneme2);
-    list.InsertNode(2, deneme3);
-    list.DisplayList();
-    cout << "/////////" << endl;
-    list.InsertNode(1, deneme4);
-    list.DisplayList();
-    cout << " /////////// " << endl;
-    list.InsertNode(4, deneme5);
-    list.DisplayList();
-    cout << " ____SİLİNDİ_____ " << endl;
-    list.DeleteNode("1030516798");
-    list.DisplayList();
-    cout << " ___ SİLİNDİ 2 " << endl;
-    list.DeleteNode("1030516799");
-    list.DisplayList();
-    
-    cout << list.FindNode("11111") << endl; */
+
     return 0;
 }
